94dac0c2a832dd95b8acab53306aeb2c43901352
JinTong <tong.jin@cdatatec.com>
2022-03-22 11:08:45 +0800

    拓展全局组件

--- src/main.js
+++ src/main.js
@@ -5,12 +5,16 @@ import '@/assets/css/global.less';
 import store from './store'
 import router from './router'
 import '@/assets/iconsvg/iconfont.css'
+import ConfigHeader from '@/components/ConfigHeader/index.vue';
+import ControlButtons from '@/components/ControlButtons/index.vue';
 
 const app = createApp(App)
 
 app.use(store)
 app.use(router)
 app.use(i18n)
+app.component(ConfigHeader);
+app.component(ControlButtons);
 
 
 app.mount('#app')
