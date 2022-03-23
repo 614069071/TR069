45a1df43713b64d5c54a8491deeabb0b66c59d64
JinTong <tong.jin@cdatatec.com>
2022-03-23 10:08:34 +0800

    添加创建任务接口

--- src/views/UpgradeTask/components/add.vue
+++ src/views/UpgradeTask/components/add.vue
@@ -1,11 +1,11 @@
 <template>
   <config-header>创建任务</config-header>
 
-  <a-form layout="vertical" :model="form">
+  <a-form layout="vertical" ref="formRef" :model="condition">
     <a-row :gutter="40">
       <a-col :span="8">
-        <a-form-item label="厂商">
-          <a-select v-model="form.value1" placeholder="please enter...">
+        <a-form-item field="manufacturer" label="厂商">
+          <a-select v-model="condition.manufacturer" placeholder="please enter...">
             <a-option label="1" value="1"></a-option>
             <a-option label="2" value="2"></a-option>
             <a-option label="3" value="3"></a-option>
@@ -13,8 +13,8 @@
         </a-form-item>
       </a-col>
       <a-col :span="8">
-        <a-form-item label="设备类型">
-          <a-select v-model="form.deviceType" placeholder="please enter...">
+        <a-form-item field="deviceType" label="设备类型">
+          <a-select v-model="condition.deviceType" placeholder="please enter...">
             <a-option label="1" value="1"></a-option>
             <a-option label="2" value="2"></a-option>
             <a-option label="3" value="3"></a-option>
@@ -22,8 +22,8 @@
         </a-form-item>
       </a-col>
       <a-col :span="8">
-        <a-form-item label="升级文件">
-          <a-select v-model="form.value1" placeholder="please enter...">
+        <a-form-item field="upgradeFileUrl" label="升级文件">
+          <a-select v-model="condition.upgradeFileUrl" placeholder="please enter...">
             <a-option label="1" value="1"></a-option>
             <a-option label="2" value="2"></a-option>
             <a-option label="3" value="3"></a-option>
@@ -31,8 +31,8 @@
         </a-form-item>
       </a-col>
       <a-col :span="8">
-        <a-form-item label="升级规则">
-          <a-select v-model="form.value1" placeholder="please enter...">
+        <a-form-item field="upgradeRuleId" label="升级规则">
+          <a-select v-model="condition.upgradeRuleId" placeholder="please enter...">
             <a-option label="1" value="1"></a-option>
             <a-option label="2" value="2"></a-option>
             <a-option label="3" value="3"></a-option>
@@ -40,8 +40,8 @@
         </a-form-item>
       </a-col>
       <a-col :span="8">
-        <a-form-item label="自动升级">
-          <a-select v-model="form.autoUpgrade" placeholder="please enter...">
+        <a-form-item field="autoUpgrade" label="自动升级">
+          <a-select v-model="condition.autoUpgrade" placeholder="please enter...">
             <a-option label="自动" value="1"></a-option>
             <a-option label="不自动" value="0"></a-option>
           </a-select>
@@ -52,22 +52,29 @@
 
   <div class="create-order-submits" style="margin: 15px 0">
     <a-space>
-      <a-button html-type="submit" @click="emit('change')">取消</a-button>
+      <a-button html-type="submit" @click="cancelHandle">取消</a-button>
       <a-button html-type="submit" type="primary" @click="handleBeforeOk">创建</a-button>
     </a-space>
   </div>
 </template>
 
 <script setup>
-import { reactive } from "vue";
+import { reactive, ref } from "vue";
 import { createUpgradeTask } from "@/services/api/jin.api";
 
 const emit = defineEmits(["change"]);
 
-const form = reactive({});
+let formRef = ref(null);
+
+const condition = reactive({ manufacturer: "", deviceType: "", upgradeFileUrl: "", upgradeRuleId: "", autoUpgrade: "" });
+
+const cancelHandle = () => {
+  emit("change");
+  formRef.value.resetFields();
+};
 
 const handleBeforeOk = () => {
-  createUpgradeTask(form)
+  createUpgradeTask(condition)
     .then(res => {
       console.log(res);
     })
